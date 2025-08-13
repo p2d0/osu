cb7aa22d2844798835acce3f3a1c395f167da534
patriot720 <cerkin-3@yandex.ru>
2025-08-13 07:53:22 +0300

    Revert "remove grouping by default"
    This reverts commit 37278a3254390a1f87f3c10b754a1cc971a52ee6.

--- osu.Game/Screens/SelectV2/BeatmapCarouselFilterGrouping.cs
+++ osu.Game/Screens/SelectV2/BeatmapCarouselFilterGrouping.cs
@@ -135,7 +135,7 @@ namespace osu.Game.Screens.SelectV2
                 return false;
 
             // In the majority case we group sets together for display.
-            return false;
+            return true;
         }
 
         private List<GroupMapping> getGroups(List<CarouselItem> items, FilterCriteria criteria)
