d446f001dc6b65739cc1f6aa71f7ea640c408b85
patriot720 <cerkin-3@yandex.ru>
2025-09-24 12:23:28 +0300

    WIP

---
+++ osu.Game/Overlays/Profile/Header/Components/PPGraph.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Humanizer;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions.LocalisationExtensions;
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Resources.Localisation.Web;
+using osu.Game.Users;
+
+namespace osu.Game.Overlays.Profile.Header.Components
+{
+    public partial class PPGraph : RankGraph
+    {
+        protected override float GetDataPointHeight(int rank) => MathF.Log(rank);
+        private const int pp_days = 88;
+
+        protected override UserGraphTooltipContent GetTooltipContent(int index, int rank)
+        {
+            int days = pp_days - index + 1;
+
+            return new UserGraphTooltipContent
+            {
+                Name = @"PP",
+                Count = rank.ToLocalisableString("#,##0"),
+                Time = days == 0 ? "now" : $"{"day".ToQuantity(days)} ago",
+            };
+        }
+    }
+}
